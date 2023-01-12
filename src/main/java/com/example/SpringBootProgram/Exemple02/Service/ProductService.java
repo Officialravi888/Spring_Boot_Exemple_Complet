ad08c4c4d46fedc652f3fb8ea347904a387dd318
Officialravi888 <youremail@yourdomain.com>
2023-01-12 16:46:28 +0530

    SpringBoot_Exemple_12/01/2023

---
+++ src/main/java/com/example/SpringBootProgram/Exemple02/Service/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.SpringBootProgram.Exemple02.Service;
+
+import com.example.SpringBootProgram.Exemple02.Model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class ProductService {
+
+    public List<Product> findAll() {
+        ArrayList<Product> products = new ArrayList<Product>();
+        products.add(new Product(13, "Mobile", "Cl89", 8990, 98));
+        return products;
+
+    }
+}
